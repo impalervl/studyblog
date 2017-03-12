8815861ad776111be8bdfd9562fdfe96c588bf48
impaler <impaler>
2017-03-12 18:10:25 +0200

    admin master page

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+
+    .scripts(['resources/assets/js/libs/bootstrap.js',
+        'resources/assets/js/libs/bootstrap.min.js',
+        'resources/assets/js/libs/jquery.js',
+        'resources/assets/js/libs/metisMenu.js',
+        'resources/assets/js/libs/sb-admin-2.js'],
+    
+        'public/js/libs.js')
+
+   .js('resources/assets/js/app.js', 'public/js')
+
+   .styles(['resources/assets/css/blog-post.css',
+       'resources/assets/css/bootstrap.css',
+       'resources/assets/css/bootstrap.min.css',
+       'resources/assets/css/font-awesome.css',
+       'resources/assets/css/metisMenu.css',
+       'resources/assets/css/sb-admin-2.css',
+       'resources/assets/css/styles.css',
+       'resources/assets/css/timeline.css'],
+
+       'public/css/libs.css');
+
+
+
